6b20c2044c5f9e2e56bcd3a1fc24cfc4c8a03856
alwayswannafly <15007159069@163.com>
2020-09-18 10:37:08 +0800

    使用原始Hooks开发Todolist小案例

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,56 +1,120 @@
 import React, {Component, useState, PureComponent, useMemo, memo, useCallback, useRef, useEffect} from 'react';
 import './App.css';
 
+let idSeq = Date.now();
 
-// class Counter extends PureComponent {
-//     render() {
-//         const {props} = this;
-//         return (
-//             <h1>{props.count}</h1>
-//         )
-//
-//     }
-// }
+const Control = memo(function Control(props) {
+    const {addTodo} = props;
+    const inputRef = useRef();
 
-function useCounter(count) {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newText = inputRef.current.value.trim();
+        if (newText.length === 0) {
+            return
+        }
+        addTodo({
+            id: ++idSeq,
+            text: newText,
+            complete: false
+        })
+        inputRef.current.value = '';
+    }
+
     return (
-        <h1>{count}</h1>
+        <div className="control">
+            <h1>todos</h1>
+            <form action="" onSubmit={onSubmit}>
+                <input ref={inputRef} type="text" className="new-todo" placeholder='what needs to be done?'/>
+            </form>
+        </div>
     )
-}
+})
 
-function useCount(defaultCount) {
-    const [count, setCount] = useState(defaultCount);
-    let it = useRef();
+const TodoItem = memo(function TodoItem(props) {
+    const {
+        todo: {
+            id, text, complete
+        },
+        toggleTodo,
+        removeTodo,
+    } = props;
+
+    const onChange = () => {
+        toggleTodo(id)
+    };
+    const onRemove = () => {
+        removeTodo(id)
+    };
+
+    return (
+        <li className="todo-item">
+            <input type="checkbox" onChange={onChange} checked={complete}/>
+            <label className={complete ? 'complete' : ''}>{text}</label>
+            <button onClick={onRemove}>&#xd7;</button>
+        </li>
+    )
+
+})
+
+const Todos = memo(function Todos(props) {
+    const {todos, toggleTodo, removeTodo} = props;
+
+    return (
+        <ul>
+            {
+                todos.map(todo => {
+                    return <TodoItem todo={todo} key={todo.id} toggleTodo={toggleTodo} removeTodo={removeTodo}/>
+                })
+            }
+        </ul>
+    )
+})
+
+const LS_KEY = '$-todos'
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    console.log(todos)
+    const addTodo = (todo) => {
+        setTodos(todos => [...todos, todo])
+    }
+
+    const removeTodo = useCallback(
+        (id) => {
+            setTodos(todos => todos.filter(todo => {
+                return todo.id !== id
+            }))
+        }, [])
+
+    const toggleTodo = useCallback(
+        (id) => {
+            setTodos(todos => todos.map(todo => {
+                return todo.id === id ? {
+                    ...todo,
+                    complete: !todo.complete
+                } : todo
+            }))
+        }
+    )
 
     useEffect(() => {
-        it.current = setInterval(() => {
-            setCount(count => count + 1)
-        }, 1000)
+        const todos = JSON.parse(localStorage.getItem(LS_KEY));
+        setTodos(todos);
     }, [])
 
     useEffect(() => {
-        if (count >= 10) {
-            clearInterval(it.current)
-        }
-    })
-    return [count, setCount]
-}
+        localStorage.setItem(LS_KEY, JSON.stringify(todos))
+    }, [todos])
 
-function App(props) {
-    const [count, setCount] = useCount(0);
-    const Counter = useCounter(count)
+
     return (
-        <div>
-            <button
-                type="button"
-                onClick={() => {
-                    setCount(count + 1)
-                }}>
-                Click({count})
-            </button>
-            {Counter}
+        <div className="todo-list">
+            <Control addTodo={addTodo}></Control>
+            <Todos removeTodo={removeTodo} toggleTodo={toggleTodo} todos={todos}></Todos>
         </div>
     )
 }
 
-export default App;
+export default TodoList;
