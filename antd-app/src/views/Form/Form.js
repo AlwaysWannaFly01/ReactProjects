3c8525dfd4c4ad18fd41e7dbe31cc13bcadcecfa
AlwaysWannaFly <15007159069@163.com>
2021-03-27 18:41:47 +0800

    初始化

---
+++ antd-app/src/views/Form/Form.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Form extends Component{
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        )
+    }
+}
+export default Form;
