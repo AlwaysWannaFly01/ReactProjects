fa684ded81cbe80cc389d3bfe35031c9f279fc68
alwayswannafly <15007159069@163.com>
2022-01-10 10:48:23 +0800

    react 实现proxy代理配置，使用setupProxy.js文件发起请求

--- react_staging/staging/src/App.js
+++ react_staging/staging/src/App.js
@@ -4,18 +4,27 @@ import axios from 'axios';
 class App extends Component {
     getData = () => {
         axios.get(
-            'http://localhost:3000/students'
+            'http://localhost:3000/api/students'
         ).then(res => {
             console.log(res)
         }).catch(err => {
             console.log(err)
         })
     }
-
+    getData2 = () => {
+        axios.get(
+            'http://localhost:3000/api2/cars'
+        ).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
     render() {
         return (
             <div>
                 <button onClick={this.getData}>获取学生数据</button>
+                <button onClick={this.getData2}>获取骑车数据</button>
             </div>
         );
     }
